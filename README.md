2a5e0825ee18a78420a20da53d244eea12459139
Ray <rlawlsdn9583@gmail.com>
2024-11-22 10:30:22 +0900

    [Init] 프로젝트 추가 세팅

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ```
+// 필수 컴포넌트 설치
 $ npx shadcn@latest init
+
 $ npx shadcn@latest add alert-dialog
 $ npx shadcn@latest add button
 $ npx shadcn@latest add popover
@@ -11,5 +13,9 @@ $ npx shadcn@latest add progress
 $ npx shadcn@latest add separator
 $ npx shadcn@latest add skeleton
 $ npx shadcn@latest add toast
+
+$ npm i sass
+$ npm i npm i @uiw/react-markdown-editor
+$ npm install @supabase/supabase-js
 ```
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { FONT_NOTOSANSKR } from "@/public/assets/font";
+import { Toaster } from "@/components/ui/toast/toaster";
 import "@/public/styles/globals.css";
+import "@/public/styles/main.scss";
 
 export const metadata: Metadata = {
   title: "TODO-BOARD 만들기",
@@ -17,6 +19,11 @@ export default function RootLayout({
       <body
         className={FONT_NOTOSANSKR.className}
       >
+        {/* <div className="page">
+          <AsideSection />
+          <main className="page__main">{children}</main>
+        </div> */}
+        <Toaster />
         {children}
       </body>
     </html>
